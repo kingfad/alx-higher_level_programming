d7fa16bdeea42c1ebdc1ee950c21392a98a42d03
kingfad <kingfad023@gmail.com>
2022-09-07 01:15:43 +0000

    update

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let last = list.length - 1;
+  let first = 0;
+  const arr = [];
+
+  while (first < list.length) {
+    arr[first] = list[last];
+    last--;
+    first++;
+  }
+
+  return arr;
